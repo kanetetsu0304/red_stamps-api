de2e0b9cfc8b5a4abcbdf8f04f581b551e95ba5b
tetsushi-kaneko <tetsushi-kaneko@bm-sms.co.jp>
2020-09-02 15:56:30 +0900

    ルーティングとコントローラ

--- app/Http/Controllers/RedStampController.php
+++ app/Http/Controllers/RedStampController.php
@@ -5,44 +5,26 @@ namespace App\Http\Controllers;
 use App\RedStamp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log as FacadesLog;
-use Log;
+// use Illuminate\Support\Facades\Log as FacadesLog;
+// use Log;
 
 class RedStampController extends Controller
 {
     public function store(Request $request)
     {
-        // if (request()->file) {
-        //     $file_name =  request()->file->getClientOriginalName();
-        //     request()->file->storeAs('public', $file_name);
-
         $redstamp = new RedStamp();
-        // $redstamp->user_id = $request->user_id;
-        // $redstamp->sanctuary_id = $request->sanctuary_id;
-        // $redstamp->date = $request->date;
-        // $redstamp->comment = $request->comment;
-        // $redstamp->image_url = $request->image_url;
-        // $redstamp->save();
-        // return response()->json($redstamp);
-
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $fileName = time() . '_redstamp_' . $image->getClientOriginalName();
             $image->storeAs('/public', $fileName);
             $redstamp->image_url = 'storage/' . $fileName;
         }
-
-        // 付帯情報の設定
         $redstamp->comment = $request->comment;
         $redstamp->date = $request->date;
         $redstamp->user_id = Auth::id();
         $redstamp->sanctuary_id = $request->sanctuary_id;
-
-        // 保存の実行
         $redstamp->save();
-
         return $redstamp;
-        // }
     }
 
     public function index()
@@ -62,25 +44,27 @@ class RedStampController extends Controller
         return $redstamp ?? abort(404);
     }
 
-    public function usersShow(string $userId,string $id)
+    public function usersShow(string $userId, string $id)
     {
         $redstamp = RedStamp::with('user', 'sanctuary')->where('user_id', $userId)->where('id', $id)->with('user', 'sanctuary')->first();
 
         return $redstamp ?? abort(404);
     }
 
-    
+
     public function update(Request $request, $id)
     {
-        // Log::debug($request);
-        // $h = "こんにちは";
-        // Log::debug($h);
         $redstamp = RedStamp::find($id);
-        $redstamp->user_id = $request->user_id;
-        $redstamp->sanctuary_id = $request->sanctuary_id;
-        $redstamp->date = $request->date;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $fileName = time() . '_redstamp_' . $image->getClientOriginalName();
+            $image->storeAs('/public', $fileName);
+            $redstamp->image_url = 'storage/' . $fileName;
+        }
         $redstamp->comment = $request->comment;
-        $redstamp->image_url = $request->image_url;
+        $redstamp->date = $request->date;
+        $redstamp->user_id = Auth::id();
+        $redstamp->sanctuary_id = $request->sanctuary_id;
         $redstamp->save();
         return $redstamp;
     }
--- routes/api.php
+++ routes/api.php
@@ -25,29 +25,20 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/redstamps/{id}', 'RedStampController@show');
     Route::put('/redstamps/{id}', 'RedStampController@update');
     Route::delete('/redstamps/{id}', 'RedStampController@destroy');
+
+    Route::post('/users/follow', 'FollowController@store');
+    Route::delete('/users/unfollow/{id}', 'FollowController@destroy');
+    Route::get('/users/followings/{id}/', 'FollowController@followings');
+    Route::get('/users/followers/{id}/', 'FollowController@followers');
+
+    Route::get('/sanctuaries', 'SanctuaryController@index');
+
+    Route::get('/users', 'UsersController@index');
+    Route::get('/users/{id}', 'UsersController@show');
+    Route::get('/usersRedstamps/{id}', 'RedStampController@usersIndex');
+    Route::get('/usersRedstamps/{userId}/{id}', 'RedStampController@usersShow');
 });
 
 Route::post('/register', 'RegisterController@register');
 Route::post('/login', 'LoginController@login');
 Route::post('/logout', 'LoginController@logout');
-
-// Route::post('/redstamps', 'RedStampController@store');
-//     Route::get('/redstamps', 'RedStampController@index');
-//     Route::get('/redstamps/{id}', 'RedStampController@show');
-//     Route::put('/redstamps/{id}', 'RedStampController@update');
-//     Route::delete('/redstamps/{id}', 'RedStampController@destroy');
-
-Route::post('/users/follow', 'FollowController@store');
-Route::delete('/users/unfollow/{id}', 'FollowController@destroy');
-Route::get('/users/followings/{id}/', 'FollowController@followings');
-Route::get('/users/followers/{id}/', 'FollowController@followers');
-
-Route::get('/sanctuaries', 'SanctuaryController@index');
-
-Route::get('/users', 'UsersController@index');
-Route::get('/users/{id}', 'UsersController@show');
-Route::get('/usersRedstamps/{id}', 'RedStampController@usersIndex');
-Route::get('/usersRedstamps/{userId}/{id}', 'RedStampController@usersShow');
-
-
-
